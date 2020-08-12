04320d56db5c4371b3161d05784dd6451bda8059
fede112 <fedebarone@gmail.com>
2020-08-12 22:19:26 +0200

    Fixed grabscreen

--- grabscreen.py
+++ grabscreen.py
@@ -1,5 +1,5 @@
 # Done by Frannecklp
-
+import time
 import cv2
 import numpy as np
 import win32gui, win32ui, win32con, win32api
@@ -40,18 +40,35 @@ def grab_screen(region=None):
 
 
 # returns target window size
-def window_size(target):
+def window_pos(target):
     toplist, winlist = [], []
     def enum_cb(hwnd, results):
         winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
     win32gui.EnumWindows(enum_cb, toplist)
 
     window = [(hwnd, title) for hwnd, title in winlist if target in title.lower()]
+    
+    
+    if (target == 'fénixao'):
+    #     # in a recent update of ao the display doesn't have a name
+    #     # so I patched the function for this specific game
+        for elem in window:
+            if elem[1] == 'FénixAO': 
+              wndsize = list(win32gui.GetWindowRect(elem[0]))
+              xsize = wndsize[2] - wndsize[0]
+              ysize = wndsize[3] - wndsize[1]
+
+        for elem in winlist:
+            hwnd = elem[0]
+            bbox = list(win32gui.GetWindowRect(hwnd))
+            if (bbox[2] - bbox[0] == xsize) and bbox[1] != 0:
+                win32gui.SetForegroundWindow(hwnd)
+                # print(elem, bbox)
+                return bbox
+    
     # just grab the hwnd for first window matching fenixao
     window = window[0]
     hwnd = window[0]
-
-    win32gui.SetForegroundWindow(hwnd)
     bbox = list(win32gui.GetWindowRect(hwnd))
-    print(bbox)
+
     return bbox
--- main.py
+++ main.py
@@ -4,66 +4,27 @@ import time
 import pyautogui
 from directkeys import PressKey, ReleaseKey, MoveCursor, LeftClick
 from directkeys import W, A, S, D, UP, DOWN, LEFT, RIGHT, CTRL, ENTER
-# from draw_lanes import draw_lanes
-from grabscreen import grab_screen, window_size
+from grabscreen import grab_screen, window_pos
 import win32gui
 
-import pytesseract
-# from pytesseract import image_to_string
 
-HECHIZOS = [0,0]
-H1 = [0,0]
-H2 = [0,0]
-INVENTARIO = [800, 190]
-INV1 = [760, 240]
-INV2 = [800, 240]
-INV3 = [840, 240]
-INV4 = [880, 240]
+# Posicion en pixeles
+INVENTARIO = (800, 190)
+INV1 = (760, 240) # rojas
+INV2 = (800, 240) # comida
+INV3 = (840, 240) # bebida
+INV4 = (880, 240)
 
+# stats (y,x)
+VIDA = (600, 815)
+MANA =  (560, 815)
+HAMBRE = (600, 940)
+SED = (630, 942)
 
-def roi(img, vertices):
-    
-    #blank mask:
-    mask = np.zeros_like(img)   
-    
-    #filling pixels inside the polygon defined by "vertices" with the fill color    
-
-    #returning the image only where mask pixels are nonzero
-    masked = cv2.bitwise_and(img, mask)
-    return masked
-
-
-
-def process_img(image):
-    original_image = image
-    # convert to gray
-    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # edge detection
-    # processed_img =  cv2.Canny(original_image, threshold1 = 50, threshold2=300)
-    # processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
-    
-    # (0, -1, 1000, 749)
-    # print(f'hambre: {processed_img[600,940]}')
-    
-    mask = np.zeros(processed_img.shape,np.uint8)
-    # stats
-    # mask[bbox[1]+500:bbox[3],bbox[0]+700:bbox[2]] = processed_img[bbox[1]+500:bbox[3],bbox[0]+700:bbox[2]]
-    
-    # screen
-    mask[bbox[1]+188:bbox[3]-40,bbox[0]+10:bbox[2]-305] = processed_img[bbox[1]+188:bbox[3]-40,bbox[0]+10:bbox[2]-305]
-    
-    # text = pytesseract.image_to_string(processed_img, config='--psm 6')
-    # print(text)
-
-    # try:
-        # l1, l2, m1,m2 = draw_lanes(original_image,lines)
-        # cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
-        # cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
-    # except Exception as e:
-        # print(str(e))
-        # pass
-
-    return mask #,original_image
+# VIDA = [815, 600]
+# MANA =  [815, 560]
+# HAMBRE = [940, 600]
+# SED = [942, 630]
 
 
 dt = 0.05
@@ -101,9 +62,41 @@ def right():
 
 
 
-# for i in list(range(3))[::-1]:
-    # print(i+1)
-    # time.sleep(1)
+def process_img(image):
+    original_image = image
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # vida, mana, hambre, sed
+    print(processed_img.shape)
+    # processed_img[VIDA[0], VIDA[1]]
+    # stats = [[],[],[],[]]
+    stats = {'vida':processed_img[VIDA], 'mana':processed_img[MANA], 'hambre': processed_img[HAMBRE], 'sed': processed_img[SED]}
+    # print(stats)
+    # exit()
+    # edge detection
+    # processed_img =  cv2.Canny(original_image, threshold1 = 50, threshold2=300)
+    # processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
+        
+    mask = np.zeros(processed_img.shape,np.uint8)
+    
+    # screen without inventory & borders
+    mask[188:711,10:692] = processed_img[188:711,10:692]
+    
+    # text = pytesseract.image_to_string(processed_img, config='--psm 6')
+    # print(text)
+
+    # try:
+        # l1, l2, m1,m2 = draw_lanes(original_image,lines)
+        # cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
+        # cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
+    # except Exception as e:
+        # print(str(e))
+        # pass
+
+    return mask, stats
+
+
 
 def move_pescador(prev_screen, screen):
     diff = np.sum(prev_screen-screen)
@@ -113,57 +106,67 @@ def move_pescador(prev_screen, screen):
         time.sleep(.2)
         return True
     return False
-        # exit()
+
+def check_vida(val):
+    print(f'val: {val}')
+    if val < 10:
+        # consume
+# def check_hambre()
+
+# def check_sed()
 
 
 
 if __name__ == '__main__':
         
-    bbox = window_size('notepad')
+    for i in list(range(3))[::-1]:
+        print(i+1)
+        time.sleep(1)
 
+    bbox = window_pos('fénixao')
+    print(bbox)
     last_time = time.time()
 
     screen = grab_screen(bbox)
-    processed_screen = process_img(screen)
+    processed_screen, stats = process_img(screen)
     prev_screen = processed_screen
+    
     while True:
 
-        # # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-        # screen = grab_screen(bbox)
-        # print('Frame took {} seconds'.format(time.time()-last_time))
-        # last_time = time.time()
+        screen = grab_screen(bbox)
+        print('Frame took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()    
+        processed_screen, stats = process_img(screen) 
 
+        check_vida(stats['vida'])
+        # check_hambre(stats['hambre'])
+        # check_sed(stats['sed'])           
+
+
+        #--------------------------------------------------------------
+        # pescador
+        if move_pescador(prev_screen, processed_screen):
+            screen = grab_screen(bbox)
+            processed_screen, stats = process_img(screen)
         
-        # processed_screen = process_img(screen)
-            
-        # if move_pescador(prev_screen, processed_screen):
-        #     screen = grab_screen(bbox)
-        #     processed_screen = process_img(screen)
-        
-        # prev_screen = processed_screen
-        # # print(new_screen.shape)
-        # cv2.imshow('window2', processed_screen)
-        # # cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+        prev_screen = processed_screen
+        #--------------------------------------------------------------
+
+
+        cv2.imshow('window2', processed_screen)
+        # cv2.imshow('window2',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         
 
         # mouse movement
-        print(bbox)
-        time.sleep(2)
-        # MoveCursor(20, 50, bbox[0], bbox[1])
-        MoveCursor(*INVENTARIO, bbox[0], bbox[1])
-        LeftClick()
-        MoveCursor(*INV1, bbox[0], bbox[1])
-        LeftClick()
-        time.sleep(0.01)
-        LeftClick()
-        # straight()
-        # time.sleep(0.5)
-        # if m1 < 0 and m2 < 0:
-        #     right()
-        # elif m1 > 0  and m2 > 0:
-        #     left()
-        # else:
-        #     straight()
+        # print(bbox)
+        # time.sleep(2)
+        # # MoveCursor(20, 50, bbox[0], bbox[1])
+        # MoveCursor(*INVENTARIO, bbox[0], bbox[1])
+        # LeftClick()
+        # MoveCursor(*INV1, bbox[0], bbox[1])
+        # LeftClick()
+        # time.sleep(0.01)
+        # LeftClick()
         
         #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
